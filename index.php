8ce546f6fc0391452e28322c16e6100fc34bd8fc
valychbreak <valychbreak@gmail.com>
2017-02-07 22:41:13 +0200

    Add redirection after any action is done

--- index.php
+++ index.php
@@ -1,108 +1,143 @@
 <?php
-	
-	error_reporting(E_ALL);
-
-	$testActionResult = "";
-	$notificationMessage = "";
-
-	function processAction($action) {
-		switch ($action) {
-			case 'stream_start':
-				onStreamActionStart();
-				break;
-
-			case 'stream_stop':
-				onStreamActionStop();
-				break;
-			
-			case 'stream_restart':
-				onStreamActionRestart();
-				break;
-
-			default:
-				# code...
-				break;
-		}
+	function redirectToIndexPage() {
+		header("Location: index.php");
 	}
 
-	function onStreamActionStart() {
-		$output = shell_exec('sh simple-script.sh');
-		$GLOBALS['notificationMessage'] = "The stream has been started". $output;
+    $timer = $_REQUEST["timer"];
+    $value = $_REQUEST["value"];
+    if(isset($timer))
+    {
+	switch ($timer)
+	{
+	case 'start':
+	    shell_exec('cd /var/apex/bin && ./timer_control start');
+	    redirectToIndexPage();
+	    break;
+	case 'stop':
+	    shell_exec('cd /var/apex/bin && ./timer_control stop');
+	    redirectToIndexPage();
+	    break;
+	case 'set':
+	    if(isset($value)) shell_exec('cd /var/apex/bin && ./timer_control set '.$value);
+	    redirectToIndexPage();
+	    break;
+	case 'increase':
+	    shell_exec('cd /var/apex/bin && ./timer_control increase');
+	    redirectToIndexPage();
+	    break;
+	case 'decrease':
+	    shell_exec('cd /var/apex/bin && ./timer_control decrease');
+	    redirectToIndexPage();
+	    break;
 	}
+    }
 
-	function onStreamActionStop() {
-		$GLOBALS['notificationMessage'] = "The stream has been stopped";
+    $extra = $_REQUEST["extra"];
+    if(isset($extra))
+    {
+	switch ($extra)
+	{
+	case 'increase':
+	    shell_exec('cd /var/apex/bin && ./extratime +1');
+	    redirectToIndexPage();
+	    break;
+	case 'decrease':
+	    shell_exec('cd /var/apex/bin && ./extratime -1');
+	    redirectToIndexPage();
+	    break;
 	}
+    }
 
-	function onStreamActionRestart() {
-		$GLOBALS['notificationMessage'] = "The stream has been restarted";
-	}
+    $score = $_REQUEST["score"];
+    if(isset($score) && isset($value))
+    {
+        shell_exec('cd /var/apex/bin && ./score '.$score.' '.$value);
+        redirectToIndexPage();
+    }
 
-	if(isset($_REQUEST["action"])) {
-		$currentAction = $_REQUEST["action"];
-		processAction($currentAction);
-	}
 ?>
+
 <html>
-	<head>
-		<meta charset="utf-8">
-		<link rel="stylesheet" href="style.css">
-		<link rel="stylesheet" href="assets/starter-template.css">
-		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
-	</head>
+    <head>
+	<meta charset="utf-8">
+	<title>Apex titles</title>
+	<style>
+	    .nav
+	    {
+		font: bold normal 5em/2em Arial,sans-serif;
+		text-shadow: 1px 1px white;
+		color: #039;
+		background-color: #eee;
+		border: 1px solid #eee;
+	    }
 
-	<body>
-	<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="navbar-brand" href="#">Navbar</a>
+	    ul.nav
+	    {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+		text-align: center;
+	    }
 
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="./index.php">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com/" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
+	    .nav li
+	    {
+		width: 100%;
+		list-style-type: none;
+		display: inline-block;
+	    }
 
-    <div class="container">
+	    .nav a
+	    {
+		display: block;
+		text-decoration: none;
+		border: 1px solid #999;
+	    }
 
-      <!-- <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div> -->
-      <div id="control-panel">
-      	<a href="./index.php?action=stream_start" class="btn btn-success">Start</a>
-      	<a href="index?action=stream_stop" class="btn btn-danger">Stop</a>
-      	<a href="index?action=stream_restart" class="btn btn-secondary">Restart</a>
-      </div>
-      <div id="notification">
-		<?php echo $notificationMessage;?>		
-	  </div>
-    </div><!-- /.container -->
+	    .nav a:hover
+	    {
+		color: red;
+		background-color: #fed;
+		border: 1px dotted red;
+	    }
+	    .nav a:active
+	    {
+		color: black;
+		background-color: #ccc;
+		border: 1px dotted #999;
+	    }
 
-    <script src="assets/jquery/jquery-3.1.1.min.js"></script>
-    <script src="assets/tether/js/tether.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.js"></script>
-	</body>
+	</style>
+    </head>
+
+    <body>
+	<ul class="nav">
+	    <li>TIMER</li>
+	    <li><a href="index.php?timer=start">START</a></li>
+	    <li><a href="index.php?timer=stop">STOP</a></li>
+
+	    <li><a href="index.php?timer=set&value=00:00">00:00</a></li>
+	    <li><a href="index.php?timer=set&value=45:00">45:00</a></li>
+	    <li><a href="index.php?timer=set&value=90:00">90:00</a></li>
+	    <li><a href="index.php?timer=set&value=105:00">105:00</a></li>
+
+	    <li><a href="index.php?timer=increase">INCREASE</a></li>
+	    <li><a href="index.php?timer=decrease">DECREASE</a></li>
+	</ul>
+
+	<ul class="nav">
+	    <li>EXTRA TIME</li>
+	    <li><a href="index.php?extra=increase">+1</a></li>
+	    <li><a href="index.php?extra=decrease">-1</a></li>
+	</ul>
+
+	<ul class="nav">
+	    <li>SCORE</li>
+	    <li><a href="index.php?score=host&value=+1">HOST +1</a></li>
+	    <li><a href="index.php?score=guest&value=+1">GUEST +1</a></li>
+
+	    <li><a href="index.php?score=host&value=-1">HOST -1</a></li>
+	    <li><a href="index.php?score=guest&value=-1">GUEST -1</a></li>
+	</ul>
+  
+    </body>
 </html>
